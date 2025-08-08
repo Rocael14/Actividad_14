59b35716388ba27af84b430e2935105782cffd9b
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-08-08 10:25:02 -0600

    Se crea el case 1

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ def menu():
     print("3. Mostrar participantes ordenados por edad")
     print("4. Salir")
 
+def quick_sort(lista):
+    if len(lista) <= 1:
+        return lista
 
+    pivote = lista[0]
+    menores = [x for x in lista[1:] if x[1]["paquetes"] < pivote[1]["paquetes"]]
+    iguales = [x for x in lista if x[1]["paquetes"] == pivote[1]["paquetes"]]
+    mayores = [x for x in lista[1:] if x[1]["paquetes"] > pivote[1]["paquetes"]]
+
+    return quick_sort(mayores) + iguales + quick_sort(menores)
+
+participantes = {}
 while True:
     menu()
     try:
@@ -13,8 +24,33 @@ while True:
         match opcion:
             case 1:
                 print("Agregar Participante")
+                cantidad_participantes = int(input("Cuantos Participantes desea agregar? : "))
+                for i in range(cantidad_participantes):
+                    dorsal = int(input(f"Ingrese el dorsal del participante #{i+1}: "))
+                    if dorsal >= 0:
+                        nombre = input(f"Ingrese el nombre del participante #{i+1}: ")
+                        edad = int(input(f"Ingrese el edad del participante #{i+1}: "))
+                        while True:
+                            seleccion_categoria = int(input(f"Seleccione la categoria del participante #{i+1}: (1. JUVENIL, 2. ADULTO O 3. MASTER)"))
+                            if seleccion_categoria == 1:
+                                categoria = "JUVENIL"
+                                break
+                            elif seleccion_categoria == 2:
+                                categoria = "ADULTO"
+                                break
+                            elif seleccion_categoria == 3:
+                                categoria = "MASTER"
+                                break
+                            else:
+                                print("Seleccione una categoria valida")
+                        participantes[dorsal] = {"nombre":nombre, "edad":edad, "categoria":categoria }
+
             case 2:
-                print("Mostrar Participantes")
+                print("Mostrar Participantes ordenados por nombre")
+                lista_participantes_nombre = list(participantes.values())
+                ordenamien
+                for dorsal, dato in lista_participantes_nombre:
+                    print(f"{dorsal}: {dato['nombre']} ({dato['categoria']}))")
             case 3:
                 print("Mostrar Participantes ordenados por edad")
             case 4:
