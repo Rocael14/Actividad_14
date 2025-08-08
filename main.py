71239aaf3626198f784f30c0af6005b9916ed12c
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-08-08 10:36:46 -0600

    Se crea el case 3, el cual tenia un problema antes porque esta pasando mi lista con values y es con items ya que necesito los pares

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ def quick_sort(lista):
         return lista
 
     pivote = lista[0]
-    menores = [x for x in lista[1:] if x[1]["paquetes"] < pivote[1]["paquetes"]]
-    iguales = [x for x in lista if x[1]["paquetes"] == pivote[1]["paquetes"]]
-    mayores = [x for x in lista[1:] if x[1]["paquetes"] > pivote[1]["paquetes"]]
+    menores = [x for x in lista[1:] if x[1]["edad"] < pivote[1]["edad"]]
+    iguales = [x for x in lista if x[1]["edad"] == pivote[1]["edad"]]
+    mayores = [x for x in lista[1:] if x[1]["edad"] > pivote[1]["edad"]]
 
     return quick_sort(mayores) + iguales + quick_sort(menores)
+
 
 participantes = {}
 while True:
@@ -31,7 +32,7 @@ while True:
                         nombre = input(f"Ingrese el nombre del participante #{i+1}: ")
                         edad = int(input(f"Ingrese el edad del participante #{i+1}: "))
                         while True:
-                            seleccion_categoria = int(input(f"Seleccione la categoria del participante #{i+1}: (1. JUVENIL, 2. ADULTO O 3. MASTER)"))
+                            seleccion_categoria = int(input(f"Seleccione la categoria del participante #{i+1}: (1. JUVENIL, 2. ADULTO O 3. MASTER) "))
                             if seleccion_categoria == 1:
                                 categoria = "JUVENIL"
                                 break
@@ -47,12 +48,13 @@ while True:
 
             case 2:
                 print("Mostrar Participantes ordenados por nombre")
-                lista_participantes_nombre = list(participantes.values())
-                ordenamien
-                for dorsal, dato in lista_participantes_nombre:
-                    print(f"{dorsal}: {dato['nombre']} ({dato['categoria']}))")
+
             case 3:
                 print("Mostrar Participantes ordenados por edad")
+                lista_participantes_edad = list(participantes.items())
+                ordenamiento = quick_sort(lista_participantes_edad)
+                for dorsal, dato in ordenamiento:
+                    print(f"{dorsal}: {dato['nombre']} ({dato['categoria']})) edad: {dato['edad']} ")
             case 4:
                 print("Gracias por utilizar el programa")
             case _:
