6d3d15acd4e08df3dcfcd6e4f7c9638bdc00d214
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-08-08 10:44:33 -0600

    Se crea otra funcion quick_sort pero dedica a los nombres y tambien se realiza que el dorsal sea unico

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ def menu():
 def quick_sort(lista):
     if len(lista) <= 1:
         return lista
-
     pivote = lista[0]
     menores = [x for x in lista[1:] if x[1]["edad"] < pivote[1]["edad"]]
     iguales = [x for x in lista if x[1]["edad"] == pivote[1]["edad"]]
     mayores = [x for x in lista[1:] if x[1]["edad"] > pivote[1]["edad"]]
 
-    return quick_sort(mayores) + iguales + quick_sort(menores)
+    return quick_sort(menores) + iguales + quick_sort(mayores)
 
+def quick_sort_nombre(lista):
+    if len(lista) <= 1:
+        return lista
+    pivote = lista[0]
+    menores = [x for x in lista[1:] if x[1]["nombre"] < pivote[1]["nombre"]]
+    iguales = [x for x in lista if x[1]["nombre"] == pivote[1]["nombre"]]
+    mayores = [x for x in lista[1:] if x[1]["nombre"] > pivote[1]["nombre"]]
+    return quick_sort_nombre(menores) + iguales + quick_sort_nombre(mayores)
 
 participantes = {}
 while True:
@@ -29,32 +36,40 @@ while True:
                 for i in range(cantidad_participantes):
                     dorsal = int(input(f"Ingrese el dorsal del participante #{i+1}: "))
                     if dorsal >= 0:
-                        nombre = input(f"Ingrese el nombre del participante #{i+1}: ")
-                        edad = int(input(f"Ingrese el edad del participante #{i+1}: "))
-                        while True:
-                            seleccion_categoria = int(input(f"Seleccione la categoria del participante #{i+1}: (1. JUVENIL, 2. ADULTO O 3. MASTER) "))
-                            if seleccion_categoria == 1:
-                                categoria = "JUVENIL"
-                                break
-                            elif seleccion_categoria == 2:
-                                categoria = "ADULTO"
-                                break
-                            elif seleccion_categoria == 3:
-                                categoria = "MASTER"
-                                break
-                            else:
-                                print("Seleccione una categoria valida")
-                        participantes[dorsal] = {"nombre":nombre, "edad":edad, "categoria":categoria }
+                        if dorsal not in participantes:
+                            nombre = input(f"Ingrese el nombre del participante #{i+1}: ")
+                            edad = int(input(f"Ingrese el edad del participante #{i+1}: "))
+                            while True:
+                                seleccion_categoria = int(input(f"Seleccione la categoria del participante #{i+1}: (1. JUVENIL, 2. ADULTO O 3. MASTER) "))
+                                if seleccion_categoria == 1:
+                                    categoria = "JUVENIL"
+                                    break
+                                elif seleccion_categoria == 2:
+                                    categoria = "ADULTO"
+                                    break
+                                elif seleccion_categoria == 3:
+                                    categoria = "MASTER"
+                                    break
+                                else:
+                                    print("Seleccione una categoria valida")
+                            participantes[dorsal] = {"nombre":nombre, "edad":edad, "categoria":categoria }
+                        else:
+                            print("El dorsal ya esta registrado")
+                    else:
+                        print("El dorsal no puede ser menor a 0")
 
             case 2:
                 print("Mostrar Participantes ordenados por nombre")
-
+                lista_participantes_nombre = list(participantes.items())
+                ordenamiento = quick_sort_nombre(lista_participantes_nombre)
+                for dorsal, dato in ordenamiento:
+                    print(f"-{dato['nombre']} (Dorsal: {dorsal}, Edad: {dato['edad']}, Categoria: {dato['categoria']})")
             case 3:
                 print("Mostrar Participantes ordenados por edad")
                 lista_participantes_edad = list(participantes.items())
                 ordenamiento = quick_sort(lista_participantes_edad)
                 for dorsal, dato in ordenamiento:
-                    print(f"{dorsal}: {dato['nombre']} ({dato['categoria']})) edad: {dato['edad']} ")
+                    print(f"-{dato['nombre']} (Dorsal: {dorsal}, Edad: {dato['edad']}, Categoria: {dato['categoria']})")
             case 4:
                 print("Gracias por utilizar el programa")
             case _:
